25393b36d30ca5542870418f1421a9580252bbeb
Stanislav Breadless <stanislavbezkor@gmail.com>
2023-05-21 16:56:53 +0200

    upd desc

--- fast-auth-lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FA3FSProtocol = exports.SchnorrProtocol = void 0;
-var schnorr_1 = require("./schnorr");
-Object.defineProperty(exports, "SchnorrProtocol", { enumerable: true, get: function () { return schnorr_1.SchnorrProtocol; } });
-var fa3fs_1 = require("./fa3fs");
-Object.defineProperty(exports, "FA3FSProtocol", { enumerable: true, get: function () { return fa3fs_1.FA3FSProtocol; } });
